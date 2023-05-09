5da5631f8fae3cf4e37b59886e8cb11fc071d92e
SergeyL95 <sergey.lutsenko08@gmail.com>
2023-05-09 11:02:17 -0400

    API tests for /createCustomer service added

--- src/test/java/step_definitions/CustomersManagementSteps.java
+++ src/test/java/step_definitions/CustomersManagementSteps.java
@@ -174,60 +174,6 @@ public class CustomersManagementSteps {
 	public void i_provide_valid_input_for_the_required_fields_and_click_save_customer_button_i_should_be_directed_to_sales_expenses_page() {
    
 	}
-
-
-	
-	static Response response;
-	static JSONObject requestBodyLogin;
-	static JSONObject requestBodyCreateCust;
-	static private String authToken;
-	DButils dbutils = new DButils();
-	
-	@Given("I am an authorized customer of Create customer webservice with following credentials")
-	public void i_am_an_authorized_customer_of_create_customer_webservice_with_following_credentials(String body) {
-		requestBodyLogin = new JSONObject(body);
-		response = given()
-	    		.body(requestBodyLogin.toString())
-	    		.contentType(ContentType.JSON)
-	    	.when()
-	    		.post(DataReader.getProperty("loginURI"));
-		
-		authToken = response.jsonPath().getString("token");
-		
-	}
-	@When("I make POST request with following request body")
-	public void i_make_post_request_with_following_request_body(String body) {
-		requestBodyCreateCust = new JSONObject(body);
-		response = given()
-	    		.header("Authorization", "Bearer " + authToken)
-	    		.contentType(ContentType.JSON)
-	    		.body(requestBodyCreateCust.toString())
-	    .when()
-	    		.post(DataReader.getProperty("createCustomerURI"));
-	    
-	    //handling redirection manually
-	    if (response.statusCode() == 302) {
-	        String redirectUrl = response.header("Location");
-	        // Manually follow the redirection
-	        response = given()
-	                .baseUri(redirectUrl)
-	                .when()
-	                .get();
-	    }
-	}
-	@Then("I should get HTTP Status Code {int}")
-	public void i_should_get_http_status_code(int expectedStatusCode) {
-		Assert.assertEquals(expectedStatusCode, response.getStatusCode());
-	}
-	@Then("a customer should be created in app database")
-	public void a_customer_should_be_created_in_app_database(DataTable data) {
-		List<String> listData = data.asList();
-		String query = "SELECT * FROM customers WHERE email = " + listData.get(1);
-		List<String> queryResult = dbutils.selectArecord(query);
-		
-		Assert.assertEquals(queryResult.get(2), listData.get(0));
-		Assert.assertEquals(queryResult.get(3), listData.get(1));
-	}
 	
 	
 	
