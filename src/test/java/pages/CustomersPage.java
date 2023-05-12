acb04c9fbcff56e7c3052c537b80ad0fdbf46a5a
Irada323 <iradaattila@gmail.com>
2023-05-12 10:32:54 -0700

    customer page commited

--- src/test/java/pages/CustomersPage.java
+++ src/test/java/pages/CustomersPage.java
@@ -11,7 +11,7 @@ public class CustomersPage {
 	public CustomersPage() {
 		PageFactory.initElements(Driver.getDriver(), this);
 	}
-	
+
 	@FindBy (xpath="//a[text()=' Customers']")
 	public WebElement customerTab;
 	
@@ -19,7 +19,7 @@ public class CustomersPage {
 	public WebElement customersPageHeader;
 	
 	@FindBy (xpath="//button[text()='Filter ']")
-	public WebElement filterButton;
+	public WebElement customerFilterButton;
 	
 	@FindBy (xpath="//button[text()=' New Customer']")
 	public WebElement addNewCustomerButton;
@@ -76,5 +76,120 @@ public class CustomersPage {
 	
 	@FindBy (xpath="//div[text()='USD - US Dollar']/following::span[1]")
 	public WebElement currencyDropDownButton;
+	
+	@FindBy (xpath="//span[text()='Field is required']")
+    public WebElement fieldisRequiredErrorMessage;
+	
+	@FindBy (xpath="//button[text()=' Save Customer']")
+	public WebElement saveCustomerButton;
+	
+	@FindBy (xpath="//span[text()='Name must have at least 3 letters.']")
+	public WebElement invalidNameFieldMessage;
+	
+	@FindBy (name="name")
+	public WebElement displayNameField;
+	
+	@FindBy (name="email")
+	public WebElement emailField;
+	
+	@FindBy (xpath="//span[text()='Incorrect Email.']")
+	public WebElement incorrectEmailMessage;
+	
+	@FindBy (xpath="//input[@type='url']")
+	public WebElement websiteField;
+	
+	@FindBy (xpath="//span[text()='Invalid url (ex: http://www.craterapp.com)']")
+	public WebElement websiteErrormessage;
+	
+	@FindBy (xpath="//h6[@class='flex items-center']")
+	public WebElement saleExpensesHeader;
+	
+	@FindBy (xpath="//button[contains(@id,'headlessui-switch')]")
+	public WebElement toggleSwitchButton;
+	
+	@FindBy (xpath="//div[text()='Customer Portal Login URL ']")
+	public WebElement customerPortalUrlHeader;
+	
+	@FindBy (xpath="//span[contains(text(),'Please copy')]")
+	public WebElement pleaseCopyMessage;
+	
+	@FindBy (xpath="//div[text()='Password ']")
+	public WebElement passwordHeader;
+	
+	@FindBy (xpath="//div[text()='Confirm Password ']")
+	public WebElement confirmPasswordHeader;
+	
+	@FindBy (name="password")
+	public WebElement customerPasswordField;
+	
+	@FindBy (name="confirm_password")
+	public WebElement customerConfirmPasswordField;
+	
+	@FindBy (xpath="//span[text()='Password must contain 8 characters']")
+	public WebElement customerPasswordErrorMessage;
+	
+	@FindBy (xpath="//span[text()='Passwords must be identical']")
+	public WebElement customerConfirmPassworderrormessage;
+	
+	@FindBy (xpath="//h6[text()='Billing Address']")
+	public WebElement billingAddressHeader;
+	
+	@FindBy (xpath="(//input[@name='address_name'])[1]")
+	public WebElement billingNameField;
+	
+	@FindBy (xpath="(//input[@type='text'])[9]")
+	public WebElement billingCountryField;
+	
+	@FindBy (xpath="//input[@name='billing.state']")
+	public WebElement billingStateField;
+	
+	@FindBy (xpath="//input[@name='billing.city']")
+	public WebElement billingCityField;
+	
+	@FindBy (xpath="//textarea[@name='billing_street1']")
+	public WebElement billingAddress1Field;
+	
+	@FindBy (xpath="//textarea[@name='billing_street2']")
+	public WebElement billingAddress2Field;
+	
+	@FindBy (xpath="(//input[@name='phone'])[2]")
+	public WebElement billingPhoneField;
+	
+	@FindBy (xpath="(//input[@name='zip'])[1]")
+	public WebElement billingZipcodeField;
+	
+	@FindBy (xpath="//h6[text()='Shipping Address']")
+	public WebElement shippingAddressHeaser;
+	
+	@FindBy (xpath="//button[text()=' Copy from Billing']")
+	public WebElement copyFromBillingButton;
+	
+	@FindBy (xpath="//div[text()='Display Name ']")
+	public WebElement customerFilterDisplayNameHeader;
+	
+	
+	@FindBy (xpath="//div[text()='Contact Name ']")
+	public WebElement customerFilterContactNameHeader;
+	
+	@FindBy (xpath="//div[text()='Phone ']")
+	public WebElement customerFilterPhoneHeader;
+	
+	@FindBy (xpath="//label[text()='Clear All']")
+	public WebElement customerFilterClearAll;
+	
+	@FindBy (xpath="//input[@name='name']")
+	public WebElement customerFilterDisplayNameField;
+	
+	@FindBy (xpath="//input[@name='phone']")
+	public WebElement customerFilterPhoneField;
+	
+	@FindBy (xpath="//span[@title='customer1']")
+	public WebElement filterResultByName;
+	
+	@FindBy (xpath="//span[text()='1234567890']")
+	public WebElement filterResultByPhone;
+	
+	@FindBy (xpath="//span[text()='No Results Found']")
+	public WebElement filterNoresultFoundMessage;
 	
 }
--- src/test/java/step_definitions/CustomersManagementSteps.java
+++ src/test/java/step_definitions/CustomersManagementSteps.java
@@ -15,6 +15,8 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import pages.CustomersPage;
+import pages.LogInPage;
+import pages.CustomersPage;
 import utilities.BrowserUtils;
 import utilities.Driver;
 
@@ -31,14 +33,15 @@ import static io.restassured.RestAssured.given;
 import java.util.List;
 
 public class CustomersManagementSteps {
-	
 	CustomersPage customersPage=new CustomersPage();
 	BrowserUtils utils=new BrowserUtils();
+	LogInPage loginPage= new LogInPage();
 	
 
 	@Given("I navigate to Customers tab")
 	public void i_navigate_to_customers_tab() {
-		
+		Driver.getDriver().get(DataReader.getProperty("appUrl"));
+		loginPage.login();
 		customersPage.customerTab.click();
 	  
 	}
@@ -48,12 +51,13 @@ public class CustomersManagementSteps {
 		Assert.assertTrue(customersPage.customersPageHeader.isDisplayed());
 	 
 	}
-	@Then("I should be able to see button titled filter")
-	public void i_should_be_able_to_see_button_titled_filter() {
-		utils.waitUntilElementVisible(customersPage.filterButton);
-		Assert.assertTrue(customersPage.filterButton.isDisplayed());
-		  
+	
+	@Then("I should be able to see button titled Filter with a filter Icon")
+	public void i_should_be_able_to_see_button_titled_filter_with_a_filter_icon() {
+		utils.waitUntilElementVisible(customersPage.customerFilterButton);
+		Assert.assertTrue(customersPage.customerFilterButton.isDisplayed());
 	}
+	
 	@Then("I should be able to see a primary button titled + New Customer")
 	public void i_should_be_able_to_see_a_primary_button_titled_new_customer() {
 	  Assert.assertTrue(customersPage.addNewCustomerButton.isDisplayed());
@@ -126,55 +130,164 @@ public class CustomersManagementSteps {
 			
 		}
 	}
-
+	//@basicInfoValidation
 	@When("I click +New Customer botton, I should directed to New Customer Page")
 	public void i_click_new_customer_botton_i_should_directed_to_new_customer_page() {
-		customersPage.addNewCustomerButton.click();
-		utils.waitUntilElementVisible(customersPage.newCustomerPageHeader);
-		Assert.assertTrue(customersPage.newCustomerPageHeader.isDisplayed());
-    
+	    customersPage.addNewCustomerButton.click();
+	    utils.waitUntilElementVisible(customersPage.newCustomerPageHeader);
+	    Assert.assertTrue(customersPage.newCustomerPageHeader.isDisplayed());
+	    
 	}
 	@When("I should be able to see basic info field")
 	public void i_should_be_able_to_see_basic_info_field() {
-		Assert.assertTrue(customersPage.basicInfoText.isDisplayed());
+	  Assert.assertTrue(customersPage.basicInfoText.isDisplayed());
 	}
 	@When("I click on Primary Currency dropdown, I should be able to see {int} different curencies")
-	public void i_click_on_primary_currency_dropdown_i_should_be_able_to_see_different_curencies(Integer int1) {
-		WebElement dropDown=customersPage.currencyDropDownButton;
+	public void i_click_on_primary_currency_dropdown_i_should_be_able_to_see_different_curencies(Integer currencies) {
+		/*WebElement dropDown=customersPage.currencyDropDownButton;
 		dropDown.click();
-		utils.waitUntilElementVisible(customersPage.currencyDropDownButton);
 		Select choose = new Select(dropDown);
 		List<WebElement> allOptions=choose.getOptions();
-		System.out.println(allOptions.size());
-		//for(WebElement element: allOptions){
-		//	System.out.println(element.getText());
-		//}
+		System.out.println(allOptions.size());*/
+			//for(WebElement element: allOptions){
+			//	System.out.println(element.getText());
+			//}
 	}
 	@When("I leave the Display Name field empty, I should see Field is required message")
 	public void i_leave_the_display_name_field_empty_i_should_see_field_is_required_message() {
-    
+	    customersPage.saveCustomerButton.click();
+	    Assert.assertTrue(customersPage.fieldisRequiredErrorMessage.isDisplayed());
 	}
-	@When("I input less the three character for the Dispay Name field,I should be able to see Name must have at least {int} letters message")
-	public void i_input_less_the_three_character_for_the_dispay_name_field_i_should_be_able_to_see_name_must_have_at_least_letters_message(Integer int1) {
-
-	}
+	
 	@When("the Email field does not follow \\(@domain.com) format, I should see Incorrect Email message")
 	public void the_email_field_does_not_follow_domain_com_format_i_should_see_incorrect_email_message() {
-   
+	  utils.actionsSendKeys(customersPage.emailField, "testdomain.com");
+	   customersPage.saveCustomerButton.click();
+	   utils.waitUntilElementVisible(customersPage.incorrectEmailMessage);
+	   Assert.assertTrue(customersPage.incorrectEmailMessage.isDisplayed());
+		
 	}
-	@When("I input less the three character for the Prefix field,I should be able to see Name must have at least {int} letters message")
-	public void i_input_less_the_three_character_for_the_prefix_field_i_should_be_able_to_see_name_must_have_at_least_letters_message(Integer int1) {
- 
-	}
+	
 	@When("Website field is not correct format,I should see Invalid url error message")
 	public void website_field_is_not_correct_format_i_should_see_invalid_url_ex_http_www_craterapp_com() {
-   
+	   utils.actionsSendKeys(customersPage.websiteField, "www.test.com");
+	   customersPage.saveCustomerButton.click();
+	   utils.waitUntilElementVisible(customersPage.websiteErrormessage);
+	   Assert.assertTrue(customersPage.websiteErrormessage.isDisplayed());
+	   customersPage.websiteField.clear();
 	}
 	@When("I provide Valid input for the required fields and click Save Customer button, I should be directed to Sales & Expenses page")
 	public void i_provide_valid_input_for_the_required_fields_and_click_save_customer_button_i_should_be_directed_to_sales_expenses_page() {
-   
+	   utils.actionsSendMultipleKeys(customersPage.displayNameField, "Jack", customersPage.emailField, "jack@gmail.com",customersPage.websiteField,"http://jack.com");
+	   customersPage.saveCustomerButton.click();
+	//   utils.waitUntilElementVisible(customersPage.saleExpensesHeader);
+	//   Assert.assertTrue(customersPage.saleExpensesHeader.isDisplayed());
 	}
 	
+
+
+@When("I click on the Toggle Switch i should view a text box titled: “Customer Portal Login URL” and “http:\\/\\/invoice.primetech-apps.com\\/crater-invoice\\/customer\\/login “ Url")
+public void i_click_on_the_toggle_switch_i_should_view_a_text_box_titled_customer_portal_login_url_and_http_invoice_primetech_apps_com_crater_invoice_customer_login_url() {
+  customersPage.toggleSwitchButton.click();
+  Assert.assertTrue(customersPage.customerPortalUrlHeader.isDisplayed());
+  
+  
+}
+@When("I click copy to clipboard button, I should see “Please copy & forward the above given URL to your customer for providing access.” message")
+public void i_click_copy_to_clipboard_button_i_should_see_please_copy_forward_the_above_given_url_to_your_customer_for_providing_access_message() {
+   Assert.assertTrue(customersPage.pleaseCopyMessage.isDisplayed());
+   
+}
+@When("i should be able to see password and confirm password field")
+public void i_should_be_able_to_see_password_and_confirm_password_field() {
+  
+	Assert.assertTrue(customersPage.passwordHeader.isDisplayed());
+}
+@When("I provide invalid data to password field,i should see Password must contain {int} characters error message")
+public void i_provide_invalid_data_to_password_field_i_should_see_password_must_contain_characters_error_message(Integer int1) {
+ utils.actionsSendKeys(customersPage.customerPasswordField, 1234567);
+ Assert.assertTrue(customersPage.customerPasswordErrorMessage.isDisplayed());
+
 	
-	
+}
+@When("I provide different password to the confirm password field I should see Passwords must be identical: password {int} confirm password {int}")
+public void i_provide_different_password_to_the_confirm_password_field_i_should_see_passwords_must_be_identical_password_confirm_password(Integer int1, Integer int2) throws InterruptedException {
+  // customersPage.customerPasswordField.clear();
+   utils.actionsSendKeys(customersPage.customerPasswordField,12345678);
+   utils.waitUntilElementVisible(customersPage.customerConfirmPasswordField);
+   utils.actionsSendKeys(customersPage.customerConfirmPasswordField, 1234567);
+   Assert.assertTrue(customersPage.customerConfirmPassworderrormessage.isDisplayed());
+   
+}
+
+@Then("I should be able to see Billing Address field")
+public void i_should_be_able_to_see_billing_address_field() {
+  Assert.assertTrue(customersPage.billingAddressHeader.isDisplayed());
+}
+@Then("I should be able to see following fields: Name, State, City, Country, Address Street1, Address Street2, Zipcode")
+public void i_should_be_able_to_see_following_fields_name_state_city_country_address_street1_address_street2_zipcode() {
+    
+	   Assert.assertTrue(customersPage.billingNameField.isDisplayed());
+	   Assert.assertTrue(customersPage.billingCountryField.isDisplayed());
+	   Assert.assertTrue(customersPage.billingStateField.isDisplayed());
+	   Assert.assertTrue(customersPage.billingCityField.isDisplayed());
+	   Assert.assertTrue(customersPage.billingAddress1Field.isDisplayed());
+	   Assert.assertTrue(customersPage.billingAddress2Field.isDisplayed());
+	   Assert.assertTrue(customersPage.billingPhoneField.isDisplayed());
+	   Assert.assertTrue(customersPage.billingZipcodeField.isDisplayed());
+}
+
+@Then("I should be able to see Shipping Address field")
+public void i_should_be_able_to_see_shipping_address_field() {
+   Assert.assertTrue(customersPage.shippingAddressHeaser.isDisplayed());
+}
+@Then("a button titled Copy from Billing")
+public void a_button_titled_copy_from_billing() {
+   Assert.assertTrue(customersPage.copyFromBillingButton.isDisplayed());
+}
+
+@When("I click on Filter Icon, I should be able to see Display Name, Contact Name, Phone and clear all link")
+public void i_click_on_filter_icon_i_should_be_able_to_see_display_name_contact_name_phone_and_clear_all_link() {
+  customersPage.customerFilterButton.click();
+  Assert.assertTrue(customersPage.customerFilterDisplayNameHeader.isDisplayed());
+  Assert.assertTrue(customersPage.customerFilterContactNameHeader.isDisplayed());
+  Assert.assertTrue(customersPage.customerFilterPhoneHeader.isDisplayed());
+  Assert.assertTrue(customersPage.customerFilterClearAll.isDisplayed());
+}
+@When("I type value in the contact name field, the matching result should return")
+public void i_type_value_in_the_contact_name_field_the_matching_result_should_return() {
+   
+utils.actionsSendKeys(customersPage.customerFilterDisplayNameField, "customer1");
+Assert.assertTrue(customersPage.filterResultByName.isDisplayed());
+}
+@When("I type value in the phone field,the matching result should return")
+public void i_type_value_in_the_phone_field_the_matching_result_should_return() {
+    utils.actionsSendKeys(customersPage.customerFilterPhoneField, 1234567890);
+    Assert.assertTrue(customersPage.filterResultByPhone.isDisplayed());
+}
+@When("I type value in the contact name and phone field,the matching result should return")
+public void i_type_value_in_the_contact_name_and_phone_field_the_matching_result_should_return() {
+	utils.actionsSendKeys(customersPage.customerFilterDisplayNameField, "customer1");
+	utils.actionsSendKeys(customersPage.customerFilterPhoneField, 1234567890);
+	Assert.assertTrue(customersPage.filterResultByName.isDisplayed());
+	 Assert.assertTrue(customersPage.filterResultByPhone.isDisplayed());
+}
+@When("I enter a value that is not exist in the database I should see the empty table with No Results Found message")
+public void i_enter_a_value_that_is_not_exist_in_the_database_i_should_see_the_empty_table_with_no_results_found_message() {
+	utils.actionsSendKeys(customersPage.customerFilterDisplayNameField, "customerX");
+	Assert.assertTrue(customersPage.filterNoresultFoundMessage.isDisplayed());
+}
+@When("I click clear all, application will display current list of customer")
+public void i_click_clear_all_application_will_display_current_list_of_customer() {
+   customersPage.customerFilterClearAll.click();
+   Assert.assertTrue(customersPage.nameColumn.isDisplayed());
+}
+@When("I click on Filter Icon again, I should be able to see Display Name, Contact Name, Phone and clear all link")
+public void i_click_on_filter_icon_again_i_should_be_able_to_see_display_name_contact_name_phone_and_clear_all_link() {
+   customersPage.customerFilterButton.click();
+   Assert.assertTrue(customersPage.customerFilterDisplayNameHeader.isDisplayed());
+   Assert.assertTrue(customersPage.customerFilterContactNameHeader.isDisplayed());
+   Assert.assertTrue(customersPage.customerFilterPhoneHeader.isDisplayed());
+   Assert.assertTrue(customersPage.customerFilterClearAll.isDisplayed());
+}
 }
